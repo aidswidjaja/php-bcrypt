7b82df97e4e5cadcc6eff238b0d1c8721061c2ba
aidswidjaja <36395320+aidswidjaja@users.noreply.github.com>
2021-12-29 22:05:02 +1100

    provide instructions

---
+++ index.php
@@ -0,0 +1,10 @@
+<h1>PHP bcrypt POC</h1>
+<p>dirt simple, just how I like it</p>
+<ul>
+    <li><a href="create_hash.php">Create</a></li>
+    <li><a href="verify_hash.php">Verify</a></li>
+</ul>
+
+<br><br>
+<hr>
+<em>2021 &copy aidswidjaja</em>
